fc559246c99ee45926012b1d844c06e1e3360c63
ADELAITBENZEKRI <adelaitbenzekri@gmail.com>
2025-04-06 19:14:03 +0100

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import io
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
